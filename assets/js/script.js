10a2bfce19bcb9b29f7cedb171ef3d11a02d36b2
AWSMProdigy <StraightAKyle@gmail.com>
2021-10-04 13:52:18 -0400

    Search feature working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,29 @@
-var searchBtn = $("#searchBtn");
+var criminal = {
+   crimName,
+   crime,
+   location,
+   mugshot
 
-function search(url){
-    fetch(url).then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-      });
 }
 
-searchBtn.on("click", function(){
-    //get value of form
+fetch("https://jailbase-jailbase.p.rapidapi.com/recent/?source_id=fl-alac", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "jailbase-jailbase.p.rapidapi.com",
+		"x-rapidapi-key": "7cecd80984mshba5b779f70a29f7p10a0c2jsn383e80719e90"
+	}
 })
+.then(response => {
+	return response.json();
+})
+.then(data=>{
+  console.log(data);
+  criminal.crimName = data.records[0].name;
+  criminal.mugshot = data.records[0].mugshot;
+  criminal.crime = data.records[0].charges[0];
+  criminal.location = data.jail.city;
+  console.log(criminal);
+})
+.catch(err => {
+	console.error(err);
+});
